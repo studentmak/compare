964bc52d8a373c1c225b251cc4facf4194b3a74d
Studentmak <maksim7956832@gmail.com>
2024-01-26 02:45:14 +0300

    -new function

--- server/loaders/index.js
+++ server/loaders/index.js
@@ -1,5 +1,4 @@
-import { convertExcelToJson } from "../parser.js";
-import  fs  from "fs"
+import { writeFilesConverterExcelToJson } from "../parser.js";
 import { Routers } from "../routers/index.js";
 import { bodyParserLoader } from "./bodyParserLoader.js";
 import { configLoader } from "./configLoader.js";
@@ -8,39 +7,6 @@ import { listeningPort } from "./port.js";
 import morgan from "morgan"
 
 
-
-
-function scandirDataDooglys () {
-    let result = []
-    fs.readdirSync('./Data/Dooglys').forEach(file =>{
-        result.push(file)
-    })
-    return result
-}
-
-function scandirDataTilda () {
-    let result = []
-    fs.readdirSync('./Data/Tilda').forEach(file =>{
-        result.push(file)
-    })
-    return result
-}
-
-function writeFiles(name, path){
-    fs.writeFile(`/Users/majarik/Desktop/12345/${name}.txt`, JSON.stringify(convertExcelToJson(path, name).map(i => ({uid:`${i.uid}`, id:`${i.id}`, name:`${i.title}`}))), err =>{
-    if (err){
-        console.error(err)
-    }
-    })
-}
-
-function writeFilesConverterExcelToJson () {
-    let filesDirDooglys = scandirDataDooglys()
-    let filesDirTilda = scandirDataTilda()
-    for(let i=0; i < filesDirTilda.length; i++){  
-        writeFiles(filesDirTilda[i].replace(/\s+/g, "'"), 'Tilda')
-    }
-}
 writeFilesConverterExcelToJson()
 
 export function loaders(app){
--- server/parser.js
+++ server/parser.js
@@ -38,19 +38,63 @@ export function convertExcelTildaToJson (){
     return value
 }
 
-export function convertExcelToJson (path, name){
+function convertExcelToJson (path, name, property){
     const result = excelToJson({
         source: fs.readFileSync(`./Data/${path}/${name}`), //Add the data file to the "Data" directory and name it "Tilda" with the extension ".xlsx"
         header: {
             rows: 1
         },
-        columnToKey: {
-            A: "uid",
-            O: "id",
-            F: "title",
-        },
+        columnToKey: property,
     }) 
     let objectName = Object.keys(result)[0]
     let value = result[objectName]
     return value
 }
+
+function scandirDataDooglys () {
+    let result = []
+    fs.readdirSync('./Data/Dooglys').forEach(file =>{
+        result.push(file)
+    })
+    return result
+}
+
+function scandirDataTilda () {
+    let result = []
+    fs.readdirSync('./Data/Tilda').forEach(file =>{
+        result.push(file)
+    })
+    return result
+}
+
+function writeFiles(name, path, property, name_category){
+    let initial_data = convertExcelToJson(path, name, property).map(i => ({uid:`${i.uid}`, category:`${i.category}`, id:`${i.id}`, name:`${i.title}`, parent_uid: `${i.parent_uid}`}))
+
+    let first_scan = initial_data.filter(i=> (i.category.toUpperCase().includes(name_category.toUpperCase())))
+    let second_scan = initial_data.filter(i => first_scan.map(i2 => i2.id).includes(i.id))
+    let third_scan = second_scan.filter(i => i.parent_uid !== 'undefined')
+    let fourth_scan = initial_data.filter(i => third_scan.map(i2 => i2.parent_uid).includes(i.parent_uid))
+    let fifth_scan = first_scan.filter(i => !fourth_scan.map(i2 => i2.parent_uid).includes(i.parent_uid))
+
+    let result = [...fourth_scan, ...fifth_scan]
+    
+    fs.writeFile(`/Users/majarik/Desktop/12345/${name_category}.txt`, JSON.stringify(result), 
+    err =>{
+    if (err){
+        console.error(err)
+    }
+    })
+}
+
+export function writeFilesConverterExcelToJson () {
+    let filesDirDooglys = scandirDataDooglys()
+    let filesDirTilda = scandirDataTilda()
+    let name_category = ['Наборы', 'Закуски', 'Горячие блюда', 'Десерты', 'Напитки', 'Салаты', 'Супы']
+    for(let i=0; i < name_category.length; i++){  
+        writeFiles(filesDirTilda[0].replace(/\s+/g, "'"), 'Tilda', {A: "uid", E: "category", O: "id",F: "title", P: "parent_uid"}, name_category[i]) //{uid:`${i.uid}`, category:`${i.category}`, id:`${i.id}`, name:`${i.title}`}
+    }
+    // for(let i=0; i < filesDirDooglys.length; i++){  
+    //     writeFiles(filesDirDooglys[i].replace(/\s+/g, "'"), 'Dooglys', {A: "id", C: "title"}) //{id:`${i.id}`, name:`${i.title}`}
+    // }
+}
+
