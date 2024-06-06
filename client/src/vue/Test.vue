9d0dcdda1c5e34c4a259747053ecfe8b2976d38e
Studentmak <maksim7956832@gmail.com>
2024-06-06 23:43:54 +0300

    1

--- client/src/vue/Test.vue
+++ client/src/vue/Test.vue
@@ -2,7 +2,6 @@
     <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <title>Мой первый сайт на html</title>
-  <link rel="stylesheet" href="D:\15 группа\Красильников\style.css" type="text/css" />
 </head>
 <body>
     <!-- Основной блок сайта -->
@@ -10,8 +9,8 @@
      
         <!-- Шапка сайта -->
         <div id="head-site">
-            <a href="#"><img src="D:\15 группа\Красильников\images\logo2.png" alt="Кофейня" title="Кофейня" /></a>
-            <img src="D:\15 группа\Красильников\images\logo2.png" alt="+7 (3537) 25-60-60" title="+7 (3537) 25-60-60" class="telefon" />
+            
+            
         </div>
          
         <!-- Верхнее меню сайта -->
@@ -39,9 +38,7 @@
  
                 <p>Согласно теории "вчувствования", разработанной Теодором Липпсом, флегматик дает эйдос. Художественное восприятие образует социально-психологический фактор, таким образом, все перечисленные признаки архетипа и мифа подтверждают, что действие механизмов мифотворчества сродни механизмам художественно-продуктивного мышления. Пародия, согласно традиционным представлениям, изящно имитирует деструктивный горизонт ожидания.</p>
                  
-                <img src="/images/img1.png" alt="" title="" style="margin-right:40px;" />
-                <img src="/images/img2.png" alt="" title="" style="margin-right:40px;" />
-                <img src="/images/img3.png" alt="" title="" />
+                
  
                 <p>Художественный ритуал, по определению, иллюстрирует метод кластерного анализа. Сангвиник, так или иначе, изменяем. Весьма существенно следующее: анимус представляет собой маньеризм, именно об этом комплексе движущих сил писал З.Фрейд в теории сублимации.</p>
  
@@ -84,7 +81,7 @@
             <div class="yellow"></div>
             <div class="p1">ООО “Кофейня” 2015г.<br/>
             г. Москва, ул Революционная 1а</div>
-            <div class="p3"><img src="/images/stat.png" alt="" title="" /></div>
+            
             <div class="p2">Создание сайтов веб-студия <a href="#">«Парампампам»</a></div>
         </div>
         <div class="myclr"></div>
@@ -108,10 +105,48 @@
 </script>
 
 <style>
-.container{
-    background-color: brown;
-    height: 100px;
-    width: 100px;
-}
+/*Каркас сайта*/
+body {padding:0px; margin:0; background:#c94b28; font-size:14px; font-family:Arial, Sans-serif;}
+#basic {width:960px; min-height:650px; margin:0 auto; padding:20px 20px 20px 20px; background:#fff;}
+img {border:0;}
+a {color:#ff9c00;}
+a:hover {text-decoration:none;}
+ 
+/*Шапка сайта*/
+#head-site {height:120px;}
+li.none-bg {background:none!important;}
+.telefon {float:right;}
+ 
+/*Верхнее меню сайта*/
+#top-menu {width:960px; height:74px; }
+
+.bg-2 {width:940px; height:8px; background:#df7904; margin:0 auto;}
+.bg-1 ul {margin:0; padding:0; list-style:none;}
+.bg-1 ul li a {display:block; height:16px; padding:24px 27px 24px 27px; color:#ff9c00; text-decoration:none; text-transform:uppercase;}
+.bg-1 ul li a:hover {color:#fff; background:#ff9c00;}
+ 
+/*Левое меню и Контент*/
+#content {width:960px; padding:20px 0 0 0; }
+ 
+/*Контент - правый блок*/
+.right { float:right; width:690px; padding:0 0 0 20px; text-align:justify;}
+.right h1 {text-transform:uppercase; color:#4c281e; font-weight:normal; margin-top:0;}
+ 
+/*Левое меню - левый блок*/
+.left {width:250px; float:left;}
+.block-nad-menu {width:240px; height:10px; background:#ff9c00;}
+.block-pod-menu {width:240px; margin:0 0 0 10px; height:10px; background:#4c281e;}
+.block-menu {width:240px; border-left:10px solid #ff9c00; background:#4c281e;}
+.left ul {margin:0; padding:10px 0 0 0; list-style:none;}
+
+ 
+ 
+/*Подвал*/
+.myclr {clear:both; float:none; width:100%; height:1px;}
+#podval {border-top:10px solid #4c281e; margin-top:20px;}
+.yellow {width:940px; height:5px; margin:0 auto; background:#ff9c00;}
+.p1 {float:left; width:300px; padding:15px 0 0 0;}
+.p2 {float:right; width:200px; text-align:right; padding:15px 20px 0 0;}
+.p3 {float:right; width:30px; padding:15px 0 0 0;}
 </style>
 
